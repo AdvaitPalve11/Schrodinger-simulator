3eb382e55c2614b6f3c10956cd981b408563992e
Advait Palve <advaitdpalve@gmail.com>
2026-04-30 23:41:11 +0530

    Add  complex number operations

--- complex.java
+++ complex.java
@@ -1,2 +1,35 @@
-public class complex
-{
+public class complex{
+
+    public double re;
+    public double im;
+
+    public complex(double R , double I){
+        re = R;
+        im = I;
+    }
+
+    public complex add(complex num){
+        return new complex(this.re + num.re , this.im + num.im);
+    }
+
+    public complex subtract(complex num){
+        return new complex(this.re - num.re , this.im - num.im);
+    }
+
+    public complex multiply(complex num){
+        return new complex(this.re * num.re - this.im * num.im , this.re * num.im + this.im * num.re);
+    } 
+
+        public complex scale(double scalar) {
+        return new complex( this.re * scalar, this.im * scalar);
+    }
+
+     public double magnitudeSquared() {
+        return re * re + im * im;
+    }
+
+    public String toString() {
+        return re + " + " + im + "i";
+    }
+    
+}
