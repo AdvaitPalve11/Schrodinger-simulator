3987657aa4fd194f1406ad80eee7711abd6c38d4
Advait Palve <advaitdpalve@gmail.com>
2026-04-30 23:41:48 +0530

    Call complex number form Main

--- main.java
+++ main.java
@@ -1,3 +1,9 @@
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+        Complex a = new Complex(1, 2);
+        Complex b = new Complex(3, 4);
+
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
 }
