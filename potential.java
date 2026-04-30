b99579b47bd52f8f889c3c22d7f15313e3c645ab
Advait Palve <advaitdpalve@gmail.com>
2026-05-01 00:11:50 +0530

    Add a potential Barrier in a 1D space field

--- potential.java
+++ potential.java
@@ -1,3 +1,19 @@
 public class potential {
-    
+    private double[] v;
+    private int N;
+
+    public potential(int n){
+       this.N = n;
+        v = new double[N];
+    }
+
+    public void createBarrier(int start , int end , double height){
+
+        for(int i = start ; i <end ; i++)
+            v[i] = height;
+    }
+
+    public double[] getV(){
+        return v;
+    }
 }
